7459fc90c6c2cc4e9ab044a22b31915524273743
Kat-Krich <katarina.krich@gmail.com>
2025-04-30 01:32:17 -0300

    Mudanças

--- Servidor_LAB/servidor.js
+++ Servidor_LAB/servidor.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-app.use(express.static('./public'));
+app.use(express.static('./public/Lab_1'));
 var server = http.createServer(app);
 server.listen(80);
 console.log("servidor rodando...");

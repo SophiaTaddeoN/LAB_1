252c49904306d9377c1ab0a0364d7af8877ed525
Kat-Krich <katarina.krich@gmail.com>
2025-05-20 13:09:45 -0300

    GET-POST-TEMPLATE

--- Servidor_LAB/servidor.js
+++ Servidor_LAB/servidor.js
@@ -1,7 +1,34 @@
 var http = require('http');
 var express = require('express');
+var colors = require('colors');
+var bodyParser = require('body-parser');
+
 var app = express();
 app.use(express.static('./public/Lab_1'));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
+
 var server = http.createServer(app);
 server.listen(80);
-console.log("servidor rodando...");
+
+console.log('Servidor rodando ...'.blue);
+
+app.get("/", (req, res) => {
+    res.redirect("projetos.html");
+});
+
+app.post('/cadastrado', function(requisicao, resposta) {
+    var nome = requisicao.body.nome;
+    var login = requisicao.body.login;
+    var senha = requisicao.body.senha;
+    var nascimento = requisicao.body.nascimento;
+    resposta.render('resposta_cadastro', {nome, login, senha, nascimento})
+})
+
+app.post('/login', function(requisicao, resposta) {
+    var login = requisicao.body.login;
+    var senha = requisicao.body.senha;
+    resposta.render('tresposta_login', {login, senha})
+})
